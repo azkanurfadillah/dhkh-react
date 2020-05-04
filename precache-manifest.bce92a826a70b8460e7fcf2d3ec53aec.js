a77e687e2cdcd069411bf31ec7eb87983abb7e0b
azkanurf <azkanurfadillah@gmial.com>
2020-05-05 00:02:35 +0700

    Updates

---
+++ precache-manifest.bce92a826a70b8460e7fcf2d3ec53aec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee00df3e54e963d8f0f867f2ff9c48a8",
+    "url": "/dhkh-react/index.html"
+  },
+  {
+    "revision": "8e8b1a79b75546e26045",
+    "url": "/dhkh-react/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "a065fa19af297766fdc8",
+    "url": "/dhkh-react/static/css/main.4ea3b18a.chunk.css"
+  },
+  {
+    "revision": "8e8b1a79b75546e26045",
+    "url": "/dhkh-react/static/js/2.f5bb2227.chunk.js"
+  },
+  {
+    "revision": "2edef021a13ae31a4909391033a43fd7",
+    "url": "/dhkh-react/static/js/2.f5bb2227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a065fa19af297766fdc8",
+    "url": "/dhkh-react/static/js/main.efb9fc5e.chunk.js"
+  },
+  {
+    "revision": "af3774423b1ff2b7198b",
+    "url": "/dhkh-react/static/js/runtime-main.3ec5048b.js"
+  },
+  {
+    "revision": "ea5232a2c1b258ebcad333bc13dc9e2d",
+    "url": "/dhkh-react/static/media/DHKH.ea5232a2.jpg"
+  },
+  {
+    "revision": "f8f4c5e6afede53f37c4cc10fc9edb7c",
+    "url": "/dhkh-react/static/media/googlemaps.f8f4c5e6.jpeg"
+  }
+]);
